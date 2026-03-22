48c7d64c2603d695e6be0cd94a4f073831fcb793
Ven <veneerickondon@gmail.com>
2026-03-23 00:19:00 +0800

    designs for HUD
    added designs for healthbar and stolen ability stack, also added icons for abilities

--- systems/HUD/player_hud.gd
+++ systems/HUD/player_hud.gd
@@ -1,14 +1,13 @@
 extends CanvasLayer
 
-@onready var WhiteBar = %WhiteBar
-@onready var BarChaser = %BarChaser
+@onready var WhiteBar = %WhiteBarTexture
+@onready var BarChaser = %BarChaserTexture
 @onready var AbilitySlot = %AbilitySlot
 @onready var AbilityIcon = %AbilityIcon
 var style 
 var stolenabilities: Array = []
 
 func _ready():
-	style = BarChaser.get_theme_stylebox("fill") as StyleBoxFlat
 	# Listen for the signal and connect it to a local function
 	Events.player_hp_updated.connect(_on_player_hp_updated)
 	Events.ability_stolen.connect(_on_ability_stolen)
@@ -19,13 +18,13 @@ func _on_player_hp_updated(current_hp: float, max_hp: float):
 	WhiteBar.max_value = max_hp
 	WhiteBar.value = current_hp
 	
-	style.bg_color = Color.GREEN.lerp(Color.RED, current_hp / max_hp)
+	BarChaser.modulate = Color.GREEN.lerp(Color.RED, current_hp / max_hp)
 	var tween = create_tween()
 	tween.tween_property(BarChaser, "value", WhiteBar.value, 0.5).set_trans(Tween.TRANS_SINE)
-	
+
 	if BarChaser.value < 3:
 		BarChaser.value = 0
-	
+
 	if WhiteBar.value < 25:
 		var tween2 = create_tween()
 		tween2.tween_property($BarContainer, "modulate:a", 0.5, 0.5)
